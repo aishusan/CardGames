a9ac49a58cc9af3d0bda51dad267686ba2058ea9
nisthakkar <nisthakkar@L-MAA-25005006.paypalcorp.com>
2016-08-09 14:32:34 +0530

    Added Game Interface

---
+++ CardGames/src/Game.java
@@ -0,0 +1,5 @@
+import java.util.ArrayList;
+
+public interface Game {
+	public int compare(ArrayList<Hand> hands);
+}
---
+++ CardGames/src/Hand.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hand {
+	List<Card> cards;
+	
+	public Hand(ArrayList<Card> cards){
+		this.cards = cards;
+	}
+	
+}
